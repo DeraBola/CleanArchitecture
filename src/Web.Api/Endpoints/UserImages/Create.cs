040794b8761aaf1ad9af78b340e074e56b5452ab
derabola <adu.deraj@gmail.com>
2025-05-21 11:14:16 -0700

    added create imageurl endpoint

---
+++ src/Web.Api/Endpoints/UserImages/Create.cs
@@ -0,0 +1,34 @@
+﻿using Application.Images.AddImage;
+using MediatR;
+using SharedKernel;
+using Web.Api.Extensions;
+using Web.Api.Infrastructure;
+
+namespace Web.Api.Endpoints.UserImages;
+
+internal sealed class Create : IEndpoint
+{
+    public sealed class Request
+    {
+        public Guid UserId { get; set; }
+        public Uri ImageUrl { get; set; }
+    }
+
+    public void MapEndpoint(IEndpointRouteBuilder app)
+    {
+        app.MapPost("users/ImageUrl", async (Request request, ISender sender, CancellationToken cancellationToken) =>
+        {
+            var command = new AddUserImageCommand
+            {
+                UserId = request.UserId,
+                ImageUrl = request.ImageUrl,
+            };
+
+            Result<Guid> result = await sender.Send(command, cancellationToken);
+
+            return result.Match(Results.Ok, CustomResults.Problem);
+        })
+        .WithTags(Tags.Images)
+        .RequireAuthorization();
+    }
+}
