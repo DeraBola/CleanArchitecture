3e029ea954f1d207f80709046d1cfcd60dff9dec
derabola <adu.deraj@gmail.com>
2025-05-22 19:08:50 -0700

    added upload image data property

---
+++ src/Application/UserImages/ImageUpload/AddUserImageCommand.cs
@@ -0,0 +1,10 @@
+﻿using Application.Abstractions.Messaging;
+using Domain.Todos;
+
+namespace Application.Images.AddImage;
+
+public sealed record AddUserImageCommand : ICommand<Guid>
+{
+    public Guid UserId { get; set; }
+    public Uri ImageUrl { get; set; }
+}
---
+++ src/Application/UserImages/ImageUpload/AddUserImageCommandHandler.cs
@@ -0,0 +1,50 @@
+﻿using System.Net;
+using System.Runtime.Intrinsics.X86;
+using Application.Abstractions.Authentication;
+using Application.Abstractions.Data;
+using Application.Abstractions.Messaging;
+using Domain.Images;
+using Domain.Users;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using SharedKernel;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
+
+namespace Application.Images.AddImage;
+
+internal sealed class AddUserImageCommandHandler(IApplicationDbContext context, 
+    IDateTimeProvider dateTimeProvider,
+    IUserContext userContext)
+    : ICommandHandler<AddUserImageCommand, Guid>
+{
+    public async Task<Result<Guid>> Handle(AddUserImageCommand command, CancellationToken cancellationToken)
+    {
+        if (userContext.UserId != command.UserId)
+        {
+            return Result.Failure<Guid>(UserErrors.Unauthorized());
+        }
+
+        User? user = await context.Users.AsNoTracking()
+          .SingleOrDefaultAsync(u => u.Id == command.UserId, cancellationToken);
+
+        if (user is null)
+        {
+            return Result.Failure<Guid>(UserErrors.NotFound(command.UserId));
+        }
+
+        var image = new UserImages
+        {
+            Id = Guid.NewGuid(),
+            UserId = user.Id,
+            ImageUrl = command.ImageUrl,
+            CreatedAt = dateTimeProvider.UtcNow
+        };
+
+        // Step 3: Add to DB and save
+        context.UserImages.Add(image);
+        await context.SaveChangesAsync(cancellationToken);
+
+        // Step 4: Return result
+        return Result.Success(image.Id);
+    }
+}
